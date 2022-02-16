cd72e5034d7309e7ff6f9f78163d8fecb8b98943
Artur Silva <artur.silva@ubots.com.br>
2022-02-15 21:00:35 -0300

    Criando estrutura da árvore e o método insert

---
+++ arvoreGenerica/src/main/java/Node.java
@@ -0,0 +1,38 @@
+public class Node<T> {
+    private T key;
+    private Node<T> left;
+    private Node<T> right;
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", esq=" + left +
+                ", dir=" + right +
+                '}';
+    }
+}
--- arvoreGenerica/src/main/java/Tree.java
+++ arvoreGenerica/src/main/java/Tree.java
@@ -1,2 +1,25 @@
-public class Tree {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Tree<T> {
+
+    private final ArrayList<String> allowedPositions = (ArrayList<String>) Arrays.asList("left", "right");
+
+
+    public Node<T> insert(Node<T> root, Node<T> newNode, String position){
+
+        if (root == null) {
+            return newNode;
+        }
+
+        if (position.toLowerCase().equals("left")) {
+            root.setLeft(
+                    insert(root.getLeft(), newNode, position));
+        } else {
+            root.setRight(
+                    insert(root.getRight(),newNode, position));
+        }
+
+        return root;
+    }
 }
